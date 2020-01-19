84692a0fc0dc281d2f8d865cab9da4dea28c668a
art-ste-web <art.ste.web@gmail.com>
2020-01-19 20:38:23 +0200

    dev

---
+++ src/main.js
@@ -0,0 +1,9 @@
+const showMockupBtn = document.querySelector(".show-mockup-btn");
+const mockupImg = document.querySelector(".mockup-img");
+showMockupBtn.onclick = showMockup();
+
+function showMockup() {
+            mockupImg.style.height = 1000+'px';
+    
+}
+console.log(showMockupBtn);
