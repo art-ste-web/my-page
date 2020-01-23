26858d2773aff0842b308b066f719f8cf44dd4be
art-ste-web <art.ste.web@gmail.com>
2020-01-23 16:34:59 +0200

    dev

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ $("#projects").on("hide.bs.collapse show.bs.collapse", e => {
   });
 
   /*scroll menu */
-$("a").click(function(){
+$(".navbar a").click(function(){
     $("body,html").animate({
       scrollTop:$("#" + $(this).data('value')).offset().top
     },1000)
